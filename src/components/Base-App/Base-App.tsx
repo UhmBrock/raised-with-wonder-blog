6a2cf10f9215fac36a0c37a964e8bc624623fa0b
Brock Sharp <Uhmbrock@hotmail.com>
2020-07-19 23:51:17 -0500

    Polished navbar, created header image

--- src/components/Base-App/Base-App.tsx
+++ src/components/Base-App/Base-App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Navbar from '../NavBar/Navbar';
+import './base-app.scss';
+
+import blog_image from '../../images/color_swatches.jpg';
+import banner_image from '../../images/rww-banner.png';
 
 interface BaseAppProps {
 
@@ -11,10 +15,31 @@ interface BaseAppState {
 
 export default class BaseApp extends React.Component<BaseAppProps, BaseAppState> {
 
-
-    render() {
+    render() { 
         return (
-            <Navbar />
-        )
+            <div>
+                <Navbar />
+                <div className="container-fluid" style={{height: '3000px' }} id="base-app-container">
+                    
+                    <div className="row mb-4">
+                        <div className="offset-lg-1 col-lg-10">
+                            { /* https://via.placeholder.com/3000x240 */ }
+                            <img id="page-header-image" className="mx-auto" src={banner_image} alt="raised with wonder banner"/>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="offset-lg-1 col-lg-6" id="blog-column">
+                            
+                            <img src={ blog_image } className="blog-featured-image" alt="blog feature"/>
+
+                        </div>
+                        <div className="offset-lg-1 col-lg-3" id="archive-column">
+                            Archive Column
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -15,8 +15,8 @@ export default class Navbar extends React.Component<NavbarProps, NavbarState> {
     render() {
         return (
             <nav className="navbar navbar-light fixed-top navbar-expand-xl"> 
-                <div className="navbar-header ml-5">
-                    <a className="navbar-brand" href="/home">
+                <div className="navbar-header">
+                    <a className="navbar-brand ml-3" href="/home">
                         raised with wonder
                     </a>
                 </div>
@@ -27,7 +27,7 @@ export default class Navbar extends React.Component<NavbarProps, NavbarState> {
                 </button>
 
                 <div className="collapse navbar-collapse">
-                    <div className="navbar-nav mx-auto" id="navbar-section-list">
+                    <div className="navbar-nav ml-auto" id="navbar-section-list"> 
                         <a className="nav-item nav-link active" href="/home">Home</a>
                         <a className="nav-item nav-link" href="/about-me">About Me</a>
                         <a className="nav-item nav-link" href="/blog">Blog</a>
@@ -35,7 +35,7 @@ export default class Navbar extends React.Component<NavbarProps, NavbarState> {
                         <a className="nav-item nav-link" href="/homeschool">Homeschool</a>
                     </div>
             
-                    <div className="navbar-nav mx-auto" id="navbar-social-media-list">
+                    <div className="navbar-nav ml-auto" id="navbar-social-media-list">
                         <a className="mx-3 social-square" href="https://www.youtube.com/channel/UCHc75m64K2PbIEjgJXZPNEg?view_as=subscriber" target="_blank" rel="noopener noreferrer">
                             <i className="fa fa-youtube-play"/>
                         </a>
@@ -48,16 +48,18 @@ export default class Navbar extends React.Component<NavbarProps, NavbarState> {
                     </div>
                 </div> 
  
-                <form className="form-inline">
-                    <span className="collapse search">
-                        <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
-                    </span>
-                    <span className="show">
-                        <button className="btn" type="button" data-toggle="collapse" data-target=".search">
-                            <span className="fa fa-search" />
-                        </button>    
-                    </span>
-                </form>
+                <div className="collapse navbar-collapse">
+                    <form className="form-inline  ml-auto">
+                        <span className="collapse search">
+                            <input className="form-control" type="search" placeholder="Search" aria-label="Search"/>
+                        </span>
+                        <span className="show">
+                            <button id="navbar-search-button" className="btn" type="button" data-toggle="collapse" data-target=".search">
+                                <span className="fa fa-search" />
+                            </button>    
+                        </span>
+                    </form>
+                </div>
             </nav>
         )
     }
